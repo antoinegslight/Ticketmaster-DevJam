5916eb9257ce57361fe7e3072512126dee8b4782
phildupuis <dupuis_philippe@hotmail.ca>
2016-04-30 15:24:26 -0400

    Affichage sur carte

--- Components/map.js
+++ Components/map.js
@@ -7,50 +7,14 @@ var directionsService = new google.maps.DirectionsService();
 function initMap() {
     directionsDisplay = new google.maps.DirectionsRenderer();
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 40.679016, lng: -73.950499},
         zoom: 8
     });
 
-    directionsDisplay.setMap(map);
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            myPosition = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map.setCenter(myPosition);
-            var marker = new google.maps.Marker({
-                position: myPosition,
-                map: map,
-                title: 'My position'
-            });
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-    var myJSON = {city : "Québec",
-        country : "Canada",
-        date : "2016-06-25",
-        latitude : "46.82495800",
-        longitude : "-71.24762000",
-        name : "Def Leppard",
-        time : "19:00:00",
-        url : "http://ticketmaster.ca/event/10005058AC4343CC",
-        venue : "Centre Videotron"}
-    var myItinerary = new roadTripItinerary();
-    myItinerary.setEventList([myJSON]);
-    myItinerary.addAllEventToMap()
-
-
-    start = new google.maps.LatLng(-34.397, 150.644);
-    end = new google.maps.LatLng(-35.0, 150.644);
-    calcRoute(start, end);
+    //start = new google.maps.LatLng(-34.397, 150.644);
+    //end = new google.maps.LatLng(-35.0, 150.644);
+    //calcRoute(start, end);
 }
 
 function addEventToMap(event){
@@ -84,6 +48,8 @@ function calcRoute(start, end) {
             console.log(data._embedded.events);
         }
     });
+
+
     var directionsDisplay = new google.maps.DirectionsRenderer();// also, constructor can get "DirectionsRendererOptions" object
     directionsDisplay.setMap(map); // map should be already initialized.
 
