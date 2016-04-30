4f9a3835711c2a628058159541a0e804f1221fb6
Antoine <aslight21@outlook.com>
2016-04-30 13:50:35 -0400

    Can choose a duration

--- Components/script.js
+++ Components/script.js
@@ -4,33 +4,44 @@ $(document).ready(function () {
     var apikey = "apikey=sRltQrA6oDxZQG1hjFAE0ioGlHjJGGL9";
     var startDateTime = "startDateTime=2016-04-30T00:00:00Z";
     var postalCode = "postalCode=G1E4L7";
-    var latlong = "latlong=46.813274,-71.212147";
     var radius = "radius=25";
     var unit = "unit=km";
     var roadTrip = [];
 
-    $.ajax({
-        type:"GET",
-        url: baseurl + "?" + apikey + "&" + startDateTime + "&" + latlong,
-        async:true,
-        dataType: "json",
-        success: function(res) {
-            console.log(res);
-            showEvents(res._embedded.events);
-            getNextEvent(res._embedded.events);
-        },
-        error: function(xhr, status, err) {
-            // This time, we do not end up here!
-        }
-    });
-
-    showEvents = function(events){
-        $.each(events, function( index, event ) {
-            console.log( index + ": " + event.name );
-        });
+    var getEvents = function(duration, iteration, latitude, longitude){
+      var latlong = "latlong=" + latitude + "," + longitude;
+      $.ajax({
+          type:"GET",
+          url: baseurl + "?" + apikey + "&" + startDateTime + "&" + latlong,
+          async:true,
+          dataType: "json",
+          success: function(res) {
+              //console.log(res);
+              setNextEvent(res._embedded.events);
+              if (iteration < duration){
+                getEvents(duration, iteration++,
+                          roadTrip[roadTrip.length-1].latitude,
+                          roadTrip[roadTrip.length-1].longitude);
+              }
+          },
+          error: function(xhr, status, err) {
+              // This time, we do not end up here!
+          }
+      });
     }
 
-    getNextEvent = function(events){
+    var planRoadTrip = function(duration, latitude, longitude){
+
+      getEvents(duration, 0, latitude, longitude);
+    }
+
+    var showEvents = function(events){
+      $.each(events, function( index, event ) {
+          console.log( index + ": " + event.name );
+      });
+    }
+
+    var setNextEvent = function(events){
       var eventPosition = Math.floor((Math.random() * 20) + 1);
       var unformatedEvent = events[eventPosition];
       var event = {
@@ -47,4 +58,6 @@ $(document).ready(function () {
       console.log(event);
       roadTrip.push(event);
     }
+
+    planRoadTrip(6, 46.813274, -71.212147);
 });
